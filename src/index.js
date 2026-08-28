ab140dd7aa027d211801d30f38ec820dbe46bef5
Martial FOE <echofield@outlook.fr>
2026-08-28 03:14:54 +0200

    Fix Alvin Workers deployment pipeline

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 export default {
   async fetch(request, env) {
-    const response = await env.ASSETS.fetch(request);
-    const contentType = response.headers.get('content-type') || '';
-
-    if (!contentType.includes('text/html')) return response;
-
-    return new HTMLRewriter()
-      .on('body', {
-        element(element) {
-          element.append('<script src="/upgrade.js?v=20260825-focus-local-v2"></script><script src="/local-leads-100.js?v=20260827-abc-local-100-v2"></script>', { html: true });
-        },
-      })
-      .transform(response);
+    return env.ASSETS.fetch(request);
   },
 };
